8349697c4a314be1ab4f898e837cb7a9ef65e34f
yash <yashplorer@gmail.com>
2018-04-29 11:25:50 -0700

    Has node server serve index.html

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const server = http.createServer(function (req, res) {
+	fs.readFile('index.html', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+	    res.write(data);
+	    res.end();
+	})
+}).listen(port);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
